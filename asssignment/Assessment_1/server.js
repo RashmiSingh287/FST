fe4ed0cc100bd11bed899f80d675b306e1fdd66c
RashmiSingh287 <rashmi.25b01010668@abes.ac.in>
2026-09-15 16:58:49 +0530

    Add students route

--- asssignment/Assessment_1/server.js
+++ asssignment/Assessment_1/server.js
@@ -55,7 +55,9 @@ const server = http.createServer((req, res) => {
             let students = [];
 
             if (fs.existsSync("students.json")) {
-                students = JSON.parse(fs.readFileSync("students.json", "utf8"));
+                students = JSON.parse(
+                    fs.readFileSync("students.json", "utf8")
+                );
             }
 
             students.push(student);
@@ -70,6 +72,39 @@ const server = http.createServer((req, res) => {
             });
 
             res.end();
+        });
+    }
+
+    // Display students
+    else if (req.url === "/students" && req.method === "GET") {
+
+        fs.readFile("students.json", "utf8", (err, data) => {
+
+            if (err) {
+                res.writeHead(500, { "Content-Type": "text/plain" });
+                res.end("Error reading students");
+                return;
+            }
+
+            const students = JSON.parse(data);
+
+            res.writeHead(200, { "Content-Type": "text/html" });
+
+            let html = "<h1>Student Records</h1>";
+
+            students.forEach((student, index) => {
+                html += `
+                    <p>
+                        ${index + 1}.
+                        Name: ${student.name},
+                        Roll: ${student.roll},
+                        Course: ${student.course},
+                        Email: ${student.email}
+                    </p>
+                `;
+            });
+
+            res.end(html);
         });
     }
 
